45bac91077c0682a0ca7b535b6864eec39c9aa2f
reandeer <reandeer_2000@hotmail.com>
2015-12-21 14:03:21 +0700

    Start

---
+++ app/src/main/java/com/beenets/suwichai_s/mytraffic/MainActivity.java
@@ -0,0 +1,13 @@
+package com.beenets.suwichai_s.mytraffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
