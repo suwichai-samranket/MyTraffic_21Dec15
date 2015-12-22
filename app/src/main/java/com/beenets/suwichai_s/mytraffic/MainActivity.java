84a94fc2862c19bd6954c7c45c8894f0d9adf604
reandeer <reandeer_2000@hotmail.com>
2015-12-22 10:26:20 +0700

    ทำ Button Controller เสร็จแล้ว

--- app/src/main/java/com/beenets/suwichai_s/mytraffic/MainActivity.java
+++ app/src/main/java/com/beenets/suwichai_s/mytraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package com.beenets.suwichai_s.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -19,7 +23,26 @@ public class MainActivity extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //Button Controller
+        buttonController();
+
     }// Main Method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.elephant);
+                buttonMediaPlayer.start();
+
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/ftDK0atcHEw"));
+                startActivity(objIntent);
+
+            } // event
+        });
+    }
 
     private void bindWidget() {
         trafficListView = (ListView) findViewById(R.id.listView);
